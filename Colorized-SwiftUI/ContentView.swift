635c9c3fbe3a6aa7159158150c7fcb4e92c58111
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-16 17:28:49 +0300

    arrangement elements

--- Colorized-SwiftUI/ContentView.swift
+++ Colorized-SwiftUI/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var red = Double.random(in: 0...255).rounded()
+    @State private var green = Double.random(in: 0...255).rounded()
+    @State private var blue = Double.random(in: 0...255).rounded()
+    
+    ///Данная обертка позволяет следить за клавиатурой
+    @FocusState private var focusedField: Field?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ///ZStack - для расширения области действия жестов скрытия клавиатуры
+        ZStack {
+            VStack(spacing: 40) {
+                ColorView(red: red, green: green, blue: blue)
+                
+                VStack {
+                    ColorSliderView(value: $red, color: .red)
+                        .focused($focusedField, equals: .red)
+                    ColorSliderView(value: $green, color: .green)
+                        .focused($focusedField, equals: .green)
+                    ColorSliderView(value: $blue, color: .blue)
+                        .focused($focusedField, equals: .blue)
+                }
+                .frame(height: 150)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button(action: previousField) {
+                            Image(systemName: "chevron.up")
+                        }
+                        Button(action: nextField) {
+                            Image(systemName: "chevron.down")
+                        }
+                        Spacer()
+                        
+                        Button("Done") {
+                            focusedField = nil
+                        }
+                    }
+                }
+                Spacer()
+            }
         }
         .padding()
+        .background(Color("Background"))
+        .onTapGesture {
+            focusedField = nil
+        }
+    }
+}
+
+extension ContentView {
+    private enum Field {
+        case red, green, blue
+    }
+    
+    private func nextField() {
+        switch focusedField {
+            case .red:
+                focusedField = .green
+            case .green:
+                focusedField = .blue
+            case .blue:
+                focusedField = .red
+            case .none:
+                focusedField = nil
+        }
+    }
+    
+    private func previousField() {
+        switch focusedField {
+            case .red:
+                focusedField = .blue
+            case .green:
+                focusedField = .red
+            case .blue:
+                focusedField = .green
+            case .none:
+                focusedField = nil
+        }
     }
 }
 

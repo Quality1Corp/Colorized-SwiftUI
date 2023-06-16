6644b210f5f4172b5f39de4e90650468cd712768
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-16 13:33:08 +0300

    Initial Commit

---
+++ Colorized-SwiftUI/Colorized_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorized_SwiftUIApp.swift
+//  Colorized-SwiftUI
+//
+//  Created by MikhaiL on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Colorized_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
